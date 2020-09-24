b837d0b7006512b762123625b9c604735c32ba2e
Sahil Singh <sahils091@gmail.com>
2020-09-24 17:54:53 -0400

    Tried Configuring '/' route for all products

--- server/server.js
+++ server/server.js
@@ -26,17 +26,48 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
 
 app.get("/", (req, res) => {
-  res.status(200).json(products);
-});
+
+   res.status(200).json(products);
+
+  //  connection.connect();
+
+  //  connection.query('SELECT * FROM all-products LIMIT 0, 10', function (error, results, fields) {
+  //    if (error) throw error;
+  //    res.send(results)
+      
+  //  });
+  //  connection.end();
+
+  //=++++ Error Message=======
+// when Calls Made one at a time 
+
+// code: 'ER_PARSE_ERROR',
+//   errno: 1064,
+//   sqlMessage: "You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'all-products' at line 1",
+//   sqlState: '42000',
+//   index: 0,
+//   sql: 'SELECT * FROM all-products;'
+
+//when Both Calls Made together 
+
+// code: 'PROTOCOL_ENQUEUE_AFTER_QUIT',
+// fatal: false  
+})
+
+
+
 
 app.get("/new", (req, res) => {
  
-  connection.connect();
+   connection.connect();
+
   connection.query('SELECT * FROM new_products LIMIT 0, 10', function (error, results, fields) {
     if (error) throw error;
     res.send(results)
+     
   });
   connection.end();
+  
 });
 
 app.get("/mens", (req, res) => {
@@ -54,3 +85,22 @@ app.post("/user/message", (req, res) => {
   userMessage.push(newMessage);
   res.status(201).json(userMessage);
 });
+
+
+
+
+
+
+//=++++ Error Message=======
+// when Calls Made one at a time 
+
+// code: 'ER_PARSE_ERROR',
+//   errno: 1064,
+//   sqlMessage: "You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'all-products' at line 1",
+//   sqlState: '42000',
+//   index: 0,
+//   sql: 'SELECT * FROM all-products;'
+
+//when Both Calls Made together 
+// code: 'PROTOCOL_ENQUEUE_AFTER_QUIT',
+// fatal: false
