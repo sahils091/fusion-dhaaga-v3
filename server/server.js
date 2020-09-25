59742a5f9ff52cb262907173c19cb89bb02f174e
Sahil Singh <sahils091@gmail.com>
2020-09-25 11:15:34 -0400

    Configured SQL calls to the dd bot the mens and womens productsproducts rendering correctly.

--- server/server.js
+++ server/server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const mysql    = require('mysql');
-const mens = require("./data/mens.json");
-const women = require("./data/women.json");
+const mysql = require("mysql");
 const userMessage = require("./data/userMessage.json");
 
 require("dotenv").config();
@@ -12,49 +10,59 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("assets"));
 
-
 const connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'rootroot',
-  database : 'fusion-data'
+  host: "localhost",
+  user: "root",
+  password: "rootroot",
+  database: "fusion-data",
 });
 
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
 
-
 app.get("/", (req, res) => {
-
-   connection.query('SELECT * FROM all_products LIMIT 0, 10', function (error, results, fields) {
-     if (error) throw error;
-     res.send(results)
-      
-   });
-  
-})
-
-
-
+  connection.query("SELECT * FROM all_products LIMIT 0, 10", function (
+    error,
+    results,
+    fields
+  ) {
+    if (error) throw error;
+    res.send(results);
+  });
+});
 
 app.get("/new", (req, res) => {
- 
-
-  connection.query('SELECT * FROM new_products LIMIT 0, 10', function (error, results, fields) {
+  connection.query("SELECT * FROM new_products LIMIT 0, 10", function (
+    error,
+    results,
+    fields
+  ) {
     if (error) throw error;
-    res.send(results)
-     
+    res.send(results);
   });
-  
 });
 
 app.get("/mens", (req, res) => {
-  res.status(200).json(mens);
+  connection.query("SELECT * FROM mens LIMIT 0, 10", function (
+    error,
+    results,
+    fields
+  ) {
+    if (error) throw error;
+    res.send(results);
+  });
 });
 
 app.get("/women", (req, res) => {
-  res.status(200).json(women);
+  connection.query("SELECT * FROM womens LIMIT 0, 10", function (
+    error,
+    results,
+    fields
+  ) {
+    if (error) throw error;
+    res.send(results);
+  });
 });
 
 app.post("/user/message", (req, res) => {
@@ -64,5 +72,3 @@ app.post("/user/message", (req, res) => {
   userMessage.push(newMessage);
   res.status(201).json(userMessage);
 });
-
-
